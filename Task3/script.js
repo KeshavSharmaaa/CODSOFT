3f75ac937cd7e3cf8b7da23facbb80291fa5eab8
KeshavSharmaaa <ksharma8941@gmail.com>
2026-02-28 11:42:31 +0530

    Task 3 added

---
+++ Task3/script.js
@@ -0,0 +1,34 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let expression = "";
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        let value = this.textContent;
+
+        if (value === "C") {
+            expression = "";
+            display.value = "";
+        }
+
+        else if (value === "=") {
+            try {
+                expression = eval(expression).toString();
+                display.value = expression;
+            } catch {
+                display.value = "Error";
+                expression = "";
+            }
+        }
+
+        else {
+            expression += value;
+            display.value = expression;
+        }
+
+    });
+
+});
